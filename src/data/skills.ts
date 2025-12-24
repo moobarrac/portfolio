6fdba3a212842dc45c4cd55b50bf9db361959bee
moobarrac <wahabajibola01@gmail.com>
2025-12-24 21:55:02 +0100

    Refactor skills data: update skill names, add new skills, and adjust class names for consistency

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,4 +1,3 @@
-// import { TbBrandReactNative } from "react-icons/tb";
 import {
   SiNextdotjs,
   SiRedux,
@@ -16,6 +15,11 @@ import {
   SiNodedotjs,
   SiExpress,
   SiMongodb,
+  SiReactquery,
+  SiJest,
+  SiTestinglibrary,
+  SiGraphql,
+  SiWebpack,
 } from "react-icons/si";
 import { IconType } from "react-icons";
 
@@ -30,45 +34,45 @@ export const skills: Skill[] = [
   {
     id: 1,
     icon: SiReact,
-    name: "Frontend Development",
+    name: "UI Engineering",
     className: "hover:bg-[#fff]",
   },
   {
     id: 2,
     icon: SiReact,
-    name: "ReactJS",
-    className: "hover:bg-[#61DBFB]",
+    name: "React",
+    className: "hover:bg-[#61DAFB]",
   },
   {
     id: 3,
-    icon: SiRedux,
-    name: "Redux",
+    icon: SiNextdotjs,
+    name: "Next.js",
     className: "hover:bg-[#00a7e5]",
   },
   {
     id: 4,
-    icon: SiNextdotjs,
-    name: "NextJS",
-    className: "hover:bg-[#a41dadd5]",
+    icon: SiTypescript,
+    name: "TypeScript",
+    className: "hover:bg-[#007acc]",
   },
   {
     id: 5,
     icon: SiJavascript,
-    name: "Javascript",
+    name: "JavaScript",
     className: "hover:bg-[#F0DB4F]",
   },
   {
     id: 6,
-    icon: SiTypescript,
-    name: "Typescript",
-    className: "hover:bg-[#007acc]",
+    icon: SiRedux,
+    name: "Redux",
+    className: "hover:bg-[#764ABC]",
   },
-  // {
-  //   id: 7,
-  //   icon: TbBrandReactNative,
-  //   name: "React Native",
-  //   className: "hover:bg-[#6180fb]",
-  // },
+  {
+    id: 7,
+    icon: SiReactquery,
+    name: "React Query",
+    className: "hover:bg-[#FF4154]",
+  },
   {
     id: 8,
     icon: SiCss3,
@@ -113,27 +117,35 @@ export const skills: Skill[] = [
   },
   {
     id: 15,
+    icon: SiNodedotjs,
+    name: "Node.js",
+    className: "hover:bg-[#3C873A]",
+  },
+  {
+    id: 16,
+    icon: SiExpress,
+    name: "Express",
+    className: "hover:bg-[#3ab71e]",
+  },
+  {
+    id: 17,
+    icon: SiMongodb,
+    name: "MongoDB",
+    className: "hover:bg-[#47A248]",
+  },
+  {
+    id: 18,
     icon: SiFirebase,
     name: "Firebase",
     className: "hover:bg-[#FFA611]",
   },
+  { id: 19, icon: SiJest, name: "Jest", className: "hover:bg-[#C21325]" },
   {
-    id: 16,
-    icon: SiNodedotjs,
-    name: "Node JS",
-    className: "hover:bg-[#ee4b4b]",
+    id: 20,
+    icon: SiTestinglibrary,
+    name: "React Testing Library",
+    className: "hover:bg-[#E33332]",
   },
-  {
-    id: 17,
-    icon: SiExpress,
-    name: "Express JS",
-    className: "hover:bg-[#3ab71e]",
-  },
-
-  {
-    id: 18,
-    icon: SiMongodb,
-    name: "MongoDB",
-    className: "hover:bg-[#FFA611]",
-  },
+  { id: 21, icon: SiGraphql, name: "GraphQL", className: "hover:bg-[#E535AB]" },
+  { id: 22, icon: SiWebpack, name: "Webpack", className: "hover:bg-[#1C78C0]" },
 ];
